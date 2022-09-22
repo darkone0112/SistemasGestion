846a420952d9a03a2aa9d8d8c987920a349b06f9
darkone0112 <darkone0112@gmail.com>
2022-09-22 18:11:06 +0200

    desde portatil

---
+++ JavaScript/alumnoObjeto/alumnoObjeto.js
@@ -0,0 +1,35 @@
+/* Varios Alumnos una nota */
+/* 
+let alumnos = []
+let t = window.prompt("Introducir cantidad de alumnos")
+for (i = 0; i < t; i++) {
+    let alumno = {
+        nombre : window.prompt("Nombre"),
+        notas : window.prompt("nota")
+        
+    }
+    alumnos.push(alumno)   
+    document.write("<br/>"+alumnos[i].nombre + " " + alumnos[i].nota)
+}
+*/
+
+/* Un alumno Varias notas */
+
+/* let alumno = {
+    nombre : window.prompt("Nombre"),
+    apellidos : window.prompt("apellidos"),
+    notas : [6]
+} 
+document.write("Nombre del alumno: " + alumno.nombre + " " + alumno.apellidos + "<br/>")
+for (i = 0 ; i < 6 ; i++) {
+    alumno.notas[i] = window.prompt("Nota " + (i+1))
+    document.write("* Nota " + (i+1) + " " + alumno.notas[i] + "<br/>")
+}
+ */
+
+/* Unir ambos tipos = varios alumnos con varias notas */
+/* ?????? */
+
+
+
+
