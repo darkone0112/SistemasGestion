6819d4469c7ccc641bcd22636badf3c057e3c2f2
darkone0112 <darkone0112@gmail.com>
2022-09-30 17:56:50 +0200

    desde portatil

--- JavaScript/DomBOM/DomBom.js
+++ JavaScript/DomBOM/DomBom.js
@@ -1,7 +1,35 @@
 document.getElementById('btn').addEventListener('click', llamar, false);
 function llamar(){
+    var e = document.getElementById("mySelect");
+    var value = e.value
+    var estilo = document.getElementById("textred").style;
+    switch (value) {
+        case "red":
+            estilo.color = "red";
+            break;
+        case "blue": 
+            estilo.color = "blue";
+        break;
+        case "green":
+            estilo.color = "green";
+        break;
+        default:
+            break;
+    }
+    var negrita = document.getElementById("negrita").checked;
+    var cursiva = document.getElementById("cursiva").checked;
+    var tachado = document.getElementById("tachado").checked;
+    if (negrita == true) {
+        document.getElementById("textred").style.fontWeight = "bold";
+    }
+    if (cursiva == true) {
+        document.getElementById("textred").style.fontStyle= "italic";
+    }
+    if (tachado == true) {
+        document.getElementById("textred").style.textDecoration = "line-through";
+    }
     /* document.write(document.getElementById('name').value); */
-    document.getElementById("textred").style.color = "red";
     let contenido = document.getElementById("name").value;
-    document.getElementById("displayText").innerHTML = contenido;
+    /* document.getElementById("displayText").innerHTML = contenido; */
+    document.getElementById("textred").innerHTML = contenido;
 }
