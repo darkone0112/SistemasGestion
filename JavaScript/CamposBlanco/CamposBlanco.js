643fd7a5f63304850b9b8d8f8e9c94c128c36f44
darkone0112 <darkone0112@gmail.com>
2022-10-06 21:39:22 +0200

    desde portatil

--- JavaScript/CamposBlanco/CamposBlanco.js
+++ JavaScript/CamposBlanco/CamposBlanco.js
@@ -5,23 +5,22 @@ function llamar() {
     input = document.getElementsByTagName("input");
     var textred = [];
     textred = document.getElementsByTagName("p");
-
-    for (let i = 0; i < input.length; i++) {
+    var cockies = document.getElementById("cockies").checked;
+    var sexo = document.getElementsByClassName("sexo").checked;
+    if(cockies == false || sexo == false){
+        alert("Se debe seleccionar un sexo y aceptar los terminos de uso");
+    }
+    for (let i = 0; i < input.length-1; i++) {
         if (input[i].value == ''){
             error = 1;
             textred[i].style.color = "red";
+            
         }else{
             textred[i].style.color = "BLACK";
+            error = 0;
         }
     }
-    if (error == 0) {
-        alert("todo bien");
-    }else{
-        alert("debes rellenar los campo");
-    }
-    
-    
-
+    alert(error);
 }
 
     
