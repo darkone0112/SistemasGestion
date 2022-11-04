62900908a8dca8ebb868edcae98124e5ca377b4b
darkone0112 <darkone0112@gmail.com>
2022-11-04 20:20:34 +0100

    portatil

---
+++ JavaScript/PokemonApi/PokemonApi.js
@@ -0,0 +1,14 @@
+document.getElementById("generar").addEventListener("click", generar, false);
+function generar(){
+    let data = ""
+    var string = ""
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then((response) => response.json())
+    .then((data) => {document.getElementById("nombre").innerHTML = data.name
+    console.log(data)
+    for (let i = 0; i < data.abilities.length; i++) {
+        document.getElementById("habilidad").innerHTML +=data.abilities[i].ability.name + "<br>";
+    }
+    });
+}
+
