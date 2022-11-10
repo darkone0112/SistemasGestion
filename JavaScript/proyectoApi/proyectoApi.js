83097111e75dd4147cd58f7260cb8b9c838bf4f8
darkone0112 <darkone0112@gmail.com>
2022-11-10 15:45:29 +0100

    portatil

--- JavaScript/proyectoApi/proyectoApi.js
+++ JavaScript/proyectoApi/proyectoApi.js
@@ -2,11 +2,19 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '1ac7eecb91mshea5302d9bb59239p16d827jsnab03720b37ec',
-		'X-RapidAPI-Host': 'call-of-duty-modern-warfare.p.rapidapi.com'
+		'X-RapidAPI-Host': 'aerodatabox.p.rapidapi.com'
 	}
 };
-
-fetch('https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/Amartin743/psn', options)
+let data = ""
+fetch('https://aerodatabox.p.rapidapi.com/flights/number/IB3016?withAircraftImage=true&withLocation=true', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	.then(data => {
+		console.log(data[0].aircraft.model)
+		document.getElementById("matricula").innerHTML = data[0].aircraft.model
+		document.getElementById("imagen").setAttribute("src", data[0].aircraft.image.url)
+		document.getElementById("altitud").innerHTML = data[0].greatCircleDistance.feet / 100
+		document.getElementById("velocidad").innerHTML = data[0].greatCircleDistance.nm + "Nm/H"
+		document.getElementById("latitud").innerHTML = data[0].departure.airport.location.lat
+		document.getElementById("longitud").innerHTML = data[0].departure.airport.location.lon
+	})
+	
